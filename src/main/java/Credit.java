869c57b11cfb4d8de62f57ab64f648520dd85779
phantomwofl <phantomwolf@yandex.ru>
2022-03-07 21:42:17 +0400

    implementation

--- src/main/java/Credit.java
+++ src/main/java/Credit.java
@@ -1,15 +1,19 @@
 public class Credit {
 
     public double getPaymentPerMonth (int money, int percent, int month) {
-
+        double paymentPerMonth = (money / 100 * percent + money) / month;
+        return paymentPerMonth;
     }
 
     public double getTotalAmount (int money, int percent, int month) {
-
+        double totalAmount = getOverpayments(money, percent, month) + money;
+        return totalAmount;
     }
 
 
     public double getOverpayments (int money, int percent, int month) {
-
+        double overPayments = (money / 100 * percent) * month;
+        return overPayments;
     }
 }
+
